398f6f8c8a6cf84c7c69cb688483dd59989518f9
Mohit-kumar123 <mk0733920@gmail.com>
2025-06-25 09:56:52 +0530

    deployment

--- Frontend/src/api/feedback.js
+++ Frontend/src/api/feedback.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/feedback';
-
+const API_URL = 'https://lia-plus-ai.onrender.com/feedback';
 export const submitFeedback = async (data) => {
   const response = await axios.post(API_URL, data);
   return response.data;
